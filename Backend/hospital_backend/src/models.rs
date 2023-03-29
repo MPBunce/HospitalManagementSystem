ffb3776cd498c1ab772840df7df3a50fb59471f5
Matthew Bunce <matthew-bunce@outlook.com>
2023-03-29 19:24:35 -0400

    done models

--- Backend/hospital_backend/src/models.rs
+++ Backend/hospital_backend/src/models.rs
@@ -22,7 +22,7 @@ pub struct AffiliatedWith {
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Procedure {
-    pub code: i32,
+    pub procedure_code: i32,
     pub name: String,
     pub cost: i32,
 }
@@ -56,40 +56,72 @@ pub struct Nurse {
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Appointment {
-
+    pub appointment_id: i32,
+    pub patient_id: i32,
+    pub prep_nurse_id: i32,
+    pub physician_id: i32,
+    pub start_time: String,
+    pub end_time: String,
+    pub examination_room: String
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Medication {
-
+    pub code: i32,
+    pub name: String,
+    pub brand: String,
+    pub description: String
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Prescribes {
-
+    pub physician_id: i32,
+    pub patient_id: i32,
+    pub medication_code: i32,
+    pub date: String,
+    pub appointment_id: i32,
+    pub dose: String,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Block {
-
+    pub floor: i32,
+    pub code: i32
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Room {
-
+    pub room_number: i32,
+    pub room_type: String,
+    pub block_floor: i32,
+    pub block_code: i32,
+    pub unavailable: bool,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct OnCall {
-
+    pub nurse_id: i32,
+    pub block_floor: i32,
+    pub block_code: i32,
+    pub start_time: String,
+    pub end_time: String,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Stay {
-
+    pub stay_id: i32,
+    pub patient_id: i32,
+    pub room_number: i32,
+    pub stay_start: String,
+    pub stay_end: String,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct Undergoes {
-
+    pub patient_id: i32,
+    pub procedure_code: i32,
+    pub stay_id: i32,
+    pub undergoes_date: String,
+    pub attending_physician_id: i32,
+    pub attending_nurse_id
 }
